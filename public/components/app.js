55d6df7a0db299145a373846c0760ab9319fe16b
mariiu <mairenn.ust@gmail.com>
2017-09-28 00:20:19 +0300

    login page test

--- public/components/app.js
+++ public/components/app.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { LoginWindow } from './login/LoginWindow';
 
-export class App extends React.Component {
+export class App extends Component {
 
     render() {
-        return <h1>aaaaa</h1>;
+        return (
+            <div>
+            <LoginWindow />
+            <BrowserRouter>
+            <div>
+                <Switch>
+                    <Route exact path='/login' component={LoginWindow}/>
+                </Switch>
+                </div>
+            </BrowserRouter>
+            </div>
+        );
     }
 }
